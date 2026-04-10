a59806b6477f12b920fff321c94596319587994b
manoj.jaiswal <mjaiswal@landolakes.com>
2026-04-10 12:55:45 +0530

    added missed file

--- src/app/OnPushandLazyLoading/highlight.ts
+++ src/app/OnPushandLazyLoading/highlight.ts
@@ -1,29 +1,28 @@
-import { Directive,ElementRef, Input, input } from '@angular/core';
+import { Directive, ElementRef, Input, input } from '@angular/core';
 import { HostBinding, HostListener } from '@angular/core';
- 
+
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-   @Input() amount!:number;
- 
-  constructor(private el: ElementRef) {
- 
-   }
-   ngOnInit() {
-     this.el.nativeElement.style.backgroundColor = this.amount > 5000 ? 'green' : 'red';
-   }
+  @Input() amount!: number;
 
-    @HostBinding('style.backgroundColor') bg='';
-   @HostListener('mouseenter') onMouseEnter() {
-     this.bg = 'lightgreen';
-   }
-   @HostListener("mousemove") onMouseMove() {
-     this.bg = 'lightblue';
-   }
-   @HostListener('mouseleave') onMouseLeave() {
-     this.bg = '';
-   }
- 
+  constructor(private el: ElementRef) {
+
+  }
+  ngOnInit() {
+    this.el.nativeElement.style.backgroundColor = this.amount > 5000 ? 'green' : 'red';
+  }
+
+  @HostBinding('style.backgroundColor') bg = '';
+  @HostListener('mouseenter') onMouseEnter() {
+    this.bg = 'lightgreen';
+  }
+  @HostListener("mousemove") onMouseMove() {
+    this.bg = 'lightblue';
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.bg = '';
+  }
+
 }
- 
